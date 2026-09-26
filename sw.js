4e0c939b173a93d8c83daa20ee14c63fc6ae708e
MisterLuxo <misterluxo.123@gmail.com>
2026-09-26 10:58:21 -0300

    Boost Capitana

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const V = 'cancha7-v21';
+const V = 'cancha7-v25';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(CORE)).then(() => self.skipWaiting())); });
+self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(CORE).then(() => c.add('sonidos/arenga.ogg').catch(() => { }))).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
